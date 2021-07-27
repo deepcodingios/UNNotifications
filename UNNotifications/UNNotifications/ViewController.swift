cfd4a2202abba9984e0864542d790ca978ab94c0
Deepu <deepu462@gmail.com>
2021-07-27 19:22:38 +0530

    Project Setup

---
+++ UNNotifications/UNNotifications/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UNNotifications
+//
+//  Created by Pradeep Reddy Kypa on 27/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @objc func registerLocal() {
+
+    }
+
+    @objc func scheduleLocal() {
+
+    }
+
+}
+
